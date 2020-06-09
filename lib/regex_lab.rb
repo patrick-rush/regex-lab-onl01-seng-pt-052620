f3496709d5a26c8c94d448a0a31e9020a0aec922
patrick-rush <patrick.a.rush@gmail.com>
2020-06-08 21:36:24 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/\b[aeiouAEIOU][a-z]*/) == nil
+        false 
+    else
+        true
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/^[A-Z].*[\.?!]$/) == nil
+        false
+    else
+        true
+    end
 end
 
 def valid_phone_number?(phone)
-
+    if phone.match(/\W?\d{3}\W?\d{3}\W?\d{4}/) == nil
+        false
+    else
+        true
+    end
 end
